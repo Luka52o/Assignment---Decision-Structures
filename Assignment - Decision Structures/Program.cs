b56822e279f342c0c0c2fd389f01cb74ad5ecf75
Luka Silic <silicluka520@gmail.com>
2023-03-08 10:01:57 -0500

    Finished the assignment

--- Assignment - Decision Structures/Program.cs
+++ Assignment - Decision Structures/Program.cs
@@ -140,78 +140,80 @@ namespace Assignment___Decision_Structures
         // PARKING MACHINE ///////////////////////////////////////////////////////////////////////////
         public static void parkingMachine()
         {
-            bool validNumEntered = false;
+            bool done = false;
             int minutesParked;
             int hoursCharged = 0;
             int price = 4;
             Console.WriteLine("Thank you for using Sam's Parking Garage! Please enter the number of minutes you parked: ");
         
-            while (validNumEntered == false)
+            while (!done)
             {
+                while (!Int32.TryParse(Console.ReadLine(), out minutesParked) || minutesParked < 0)
+                {
+                    Console.WriteLine("Please enter a valid number or a number greater than zero: ");
+                }
 
+                if (minutesParked > 0)
+                {
+                    if (minutesParked <= 60)
+                    {
+                        price = price + 2;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 1;
+                    }
+                    else if (minutesParked > 60 && minutesParked <= 120)
+                    {
+                        price = price + 4;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 2;
+                    }
+                    else if (minutesParked > 120 && minutesParked <= 180)
+                    {
+                        price = price + 6;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 3;
+                    }
+                    else if (minutesParked > 180 && minutesParked <= 240)
+                    {
+                        price = price + 8;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 4;
+                    }
+                    else if (minutesParked > 240 && minutesParked <= 300)
+                    {
+                        price = price + 10;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 5;
+                    }
+                    else if (minutesParked > 300 && minutesParked <= 360)
+                    {
+                        price = price + 12;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 6;
+                    }
+                    else if (minutesParked > 360 && minutesParked <= 420)
+                    {
+                        price = price + 14;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 7;
+                    }
+                    else if (minutesParked > 420)
+                    {
+                        price = price + 16;
+                        Console.WriteLine("You owe $" + price + ".");
+                        hoursCharged = 8;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Here is your receipt: ");
+                Console.WriteLine("MINUTES PARKED .................... " + minutesParked);
+                Console.WriteLine("HOURS CHARGED  .................... " + hoursCharged);
+                Console.WriteLine("AMOUNT OWING   .................... $" + price);
+                Console.WriteLine("THANK YOU FOR USING SAM'S PARKING GARAGE");
+                done = true;
             }
-            while (!Int32.TryParse(Console.ReadLine(), out minutesParked))
-                Console.WriteLine("Please enter a valid number: ");
+            
 
-         
-            if (minutesParked < 0)
-                Console.WriteLine("Please enter a number greater than zero: ");
-
-            if (minutesParked <= 60)
-            {
-                price = price + 2;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 1;
-            }
-            else if (minutesParked > 60 && minutesParked <= 120)
-            {
-                price = price + 4;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 2;
-            }
-            else if (minutesParked > 120 && minutesParked <= 180)
-            {
-                price = price + 6;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 3;
-            }
-            else if (minutesParked > 180 && minutesParked <= 240)
-            {
-                price = price + 8;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 4;
-            }
-            else if (minutesParked > 240 && minutesParked <= 300)
-            {
-                price = price + 10;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 5;
-            }
-            else if (minutesParked > 300 && minutesParked <= 360)
-            {
-                price = price + 12;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 6;
-            }
-            else if (minutesParked > 360 && minutesParked <= 420)
-            {
-                price = price + 14;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 7;
-            }
-            else if (minutesParked > 420)
-            {
-                price = price + 16;
-                Console.WriteLine("You owe $" + price + ".");
-                hoursCharged = 8;
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Here is your receipt: ");
-            Console.WriteLine("MINUTES PARKED .................... " + minutesParked);
-            Console.WriteLine("HOURS CHARGED  .................... " + hoursCharged);
-            Console.WriteLine("AMOUNT OWING   .................... $" + price);
-            Console.WriteLine("THANK YOU FOR USING SAM'S PARKING GARAGE");
         }
 
 
